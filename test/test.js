046023ca854c54a75f2c6ad5a206358394fe5c8d
Jihann <1244098192@qq.com>
2015-04-17 11:50:44 +0800

    perfect ejsblog

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
 /**
  * Created by JBH on 2015/4/7.
  */
+
+console.log("----------------Are you sure test?---------------");
---
+++ test/test_async.js
@@ -0,0 +1,33 @@
+/**
+ * Created by JBH on 2015/4/17
+ */
+var async = require('async');
+var username = "Jihann";
+var email = "XXX@XXX.com";
+
+//waterfall(tasks, [callback])
+async.waterfall([
+    function(callback) {
+        callback(null, username, email);
+    },
+    function(username, email, callback) {
+        console.log("-------------- info message start --------------");
+        console.log("-------------username: " + username);
+        console.log("-------------email: " +email);
+        var userInfo = "hello " + username + ",this is my email: " + email;
+        callback(null, userInfo);
+    },
+    function(userInfo, callback) {
+        console.log("----------------userInfo: " + userInfo);
+        var final = "congratulations";
+        callback(null, final);
+    },
+    function(final, callback) {
+        var end = final + "\t" + username;
+        callback(null, end);
+    }
+], function(err, end) {
+    console.log("-------------end: " + end);
+    console.log("-------------- info message end --------------");
+});
+
